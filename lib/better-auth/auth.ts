414e65e7aa6b8b46c5fbe4dc18d8ed6eba0f712d
Prabuddha Narayan Datta <prabuddha1749@gmail.com>
2025-12-08 15:56:28 +0530

    Implemented basic signin page

--- lib/better-auth/auth.ts
+++ lib/better-auth/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@/generated/prisma/client";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -37,4 +38,6 @@ export const auth = betterAuth({
   session: {
     expiresIn: 60 * 60 * 24 * 30, // 30 days
   },
+
+  plugins: [nextCookies()],
 });
