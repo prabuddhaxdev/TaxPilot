2c3132a17142226b2b73109806b37ba0cf721b82
prabuddhaxdev <prabuddha1749@gmail.com>
2025-12-08 15:52:24 +0530

    Implemented basic signin page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/better-auth/client";
 
-const SignInPage = () => {
+const SignInSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8).max(16),
+});
+
+export default function SignInPage() {
+  const router = useRouter();
+
+  const form = useForm({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof SignInSchema>) => {
+    const res = await authClient.signIn.email(values);
+    if (res.error) {
+      toast.error(res.error.message);
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
-    <div>SignInPage</div>
-  )
-}
+    <div className="max-w-sm mx-auto pt-20">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            name="email"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-export default SignInPage
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className="w-full" type="submit">
+            Sign In
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
