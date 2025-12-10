e138ff1814fcd6247671648c6a1e2f34f6554eeb
prabuddhaxdev <prabuddha1749@gmail.com>
2025-12-10 23:58:30 +0530

    Completed SignIn Page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,93 +1,14 @@
-"use client";
+import { SignInForm } from "@/components/signin-form";
+import type { Metadata } from "next";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { useForm } from "react-hook-form";
-import z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { toast } from "sonner";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/better-auth/auth-client";
-import { useState } from "react";
+export const metadata: Metadata = {
+  title: "Sign in",
+};
 
-
-const SignInSchema = z.object({
-  email: z.email({ message: "Please enter a valid email" }),
-  password: z.string().min(8).max(16),
-});
-
-export default function SignInPage() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const redirect = searchParams.get("redirect");
-
-
-  const form = useForm({
-    resolver: zodResolver(SignInSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const onSubmit = async (values: z.infer<typeof SignInSchema>) => {
-    const result = await authClient.signIn.email(values);
-    if (result.error) {
-      toast.error(result.error.message);
-    } else {
-      router.push("/");
-    }
-  };
-
+export default function SignIn() {
   return (
-    <div className="flex min-h-screen items-center justify-center p-4">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <FormField
-            name="email"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            name="password"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input type="password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <Button className="w-full" type="submit">
-            Sign In
-          </Button>
-        </form>
-      </Form>
-    </div>
+    <main className="flex min-h-svh items-center justify-center px-4">
+      <SignInForm />
+    </main>
   );
 }
