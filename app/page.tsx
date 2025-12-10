50e6b9e5b28e57e1bdc99420cdd8e6d889a57ceb
prabuddhaxdev <prabuddha1749@gmail.com>
2025-12-10 23:15:23 +0530

    Add Dark Mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div>
       <h1>Hello World</h1>
       <Button>Click me</Button>
+      <ModeToggle/>
     </div>
   );
 }
---
+++ lib/validation.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const passwordSchema = z
+  .string()
+  .min(1, { message: "Password is required" })
+  .min(8, { message: "Password must be at least 8 characters" })
+  .regex(/[^A-Za-z0-9]/, {
+    message: "Password must contain at least one special character",
+  })
+  .max(16, { message: 'Maximum Password length is 16 characters'});
